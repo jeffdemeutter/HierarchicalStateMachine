5a49309c21fc37b0c0824e5c04f22ddba3e7fd81
Jeff DM <jeffke.de.meutter@gmail.com>
2023-07-19 23:50:39 +0200

    initial commit new version

---
+++ src/State.h
@@ -0,0 +1,5 @@
+enum class State {
+    stand,
+    crouch,
+    prone
+};
---
+++ src/StateMachine.h
@@ -0,0 +1,82 @@
+#include <functional>
+#include <iostream>
+
+template <typename T>
+class StateMachine
+{
+    static_assert(std::is_enum<T>::value,"Type provided in StateMachine was not an Enum");
+    struct TransitionContext
+    {
+        T to;
+        std::function<bool()> condition;
+    };
+
+    struct StateContext
+    {
+        std::function<void()> onStart;
+        std::function<void()> onUpdate;
+        std::function<void()> onStop;
+
+        std::vector<TransitionContext> transitions;
+    };
+    
+public:
+    StateMachine(T startState);
+    ~StateMachine() = default;
+    StateMachine(const StateMachine&) = delete;
+    StateMachine(StateMachine&&) noexcept  = delete;
+    StateMachine& operator=(const StateMachine&) = delete;
+    StateMachine& operator=(StateMachine&&) noexcept  = delete;
+
+    StateMachine& AddStateContext(T state, std::function<void()> onStart, std::function<void()> onUpdate, std::function<void()> onStop);
+    StateMachine& AddTransition(T from, T to, std::function<bool()> condition);
+
+    void Update();
+
+private:
+    std::unordered_map<T, StateContext> mStateContexts;
+    T mCurrentState;
+};
+
+
+
+
+template <typename T> 
+StateMachine<T>::StateMachine(T startState)
+    : mCurrentState(startState)
+{
+}
+
+template <typename T>
+StateMachine<T>& StateMachine<T>::AddStateContext(T state, std::function<void()> onStart, std::function<void()> onUpdate, std::function<void()> onStop)
+{
+    mStateContexts[state] = StateContext(onStart, onUpdate, onStop);
+
+    return *this;
+}
+
+template <typename T>
+StateMachine<T>& StateMachine<T>::AddTransition(T from, T to, std::function<bool()> condition)
+{
+    if (from != to)
+        mStateContexts[from].transitions.emplace_back(TransitionContext{to, condition});
+    
+    return *this;
+}
+
+template <typename T>
+void StateMachine<T>::Update()
+{
+    for (const auto& t : mStateContexts[mCurrentState].transitions)
+    {
+        if (mCurrentState == t.to)
+            continue;
+
+        if (!t.condition())
+            continue;
+
+        mCurrentState = t.to;
+
+        std::cout << "CurrentState is " << (int)mCurrentState << '\n';
+    }   
+}
