7474b6d36ae8b65cd8dd833946a58d00f3d2fb43
Jeff DM <jeffke.de.meutter@gmail.com>
2023-07-23 03:28:59 +0200

    cleaned up

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,17 @@
 int main(int argc, char const *argv[])
 { 
     std::string buttonEntered;
-
+    
 
     StateMachine sm{State::stand};
-    sm.AddStateContext(State::stand, []{ std::cout << "Stood up\n"; }, []{ std::cout << "Standing\n"; }, []{ std::cout << "Stopped standing"; })
+    sm.AddStateContext(State::stand, []{ std::cout << "Stood up\n"; }, []{ std::cout << "Standing\n"; }, []{ std::cout << "Stopped standing\n"; })
       .AddTransition(State::stand , State::prone , [&buttonEntered]{ return buttonEntered[0] == 'x'; })
-      .AddTransition(State::stand , State::crouch, [&buttonEntered]{ return buttonEntered[0] == 'c'; });
+      .AddTransition(State::stand , State::crouch, [&buttonEntered]{ return buttonEntered[0] == 'c'; })
 
-    sm.AddTransition(State::crouch, State::stand , [&buttonEntered]{ return buttonEntered[0] == 's'; })
+      .AddStateContext(State::crouch, []{ std::cout << "Crouched\n"; }, []{ std::cout << "crouching\n"; }, []{ std::cout << "Stopped crouching\n"; })
+      .AddTransition(State::crouch, State::stand , [&buttonEntered]{ return buttonEntered[0] == 's'; })
+
+      .AddStateContext(State::prone, []{ std::cout << "proned\n"; }, []{ std::cout << "proning\n"; }, []{ std::cout << "Stopped proning\n"; })
       .AddTransition(State::prone , State::stand , [&buttonEntered]{ return buttonEntered[0] == 's'; });    
     
 
