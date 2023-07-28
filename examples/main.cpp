5c31980a399a0267bc17e778c24c1184ae351de1
Tboske <jeffke.de.meutter@gmail.com>
2023-07-28 23:26:03 +0200

    formatted files

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,142 +1,141 @@
 #include <iostream>
 #include <thread>
 
-#include"../include/StateMachine.h"
+#include "../include/StateMachine.h"
 
-// an enum is mandatory, you need to pass this on to the statemachine constructor
-enum class State {
-    stand,
-    crouch,
-    prone
-};
+// an enum is mandatory, you need to pass this on to the statemachine
+// constructor
+enum class State { stand, crouch, prone };
 
 void BasicExample();
 void BasicWithUsingExample();
 void StateCallBackExamples();
 void UpdateMultiThreadedExample();
 
+int main(int argc, char const *argv[]) {
+  // uncomment the examples below to test them out yourself
 
-int main(int argc, char const *argv[])
-{ 
-    // uncomment the examples below to test them out yourself
-
-    BasicExample();
-    // BasicWithUsingExample();
-    // StateCallBackExamples();
-    // UpdateMultiThreadedExample();
-    return 0;
+  BasicExample();
+  // BasicWithUsingExample();
+  // StateCallBackExamples();
+  // UpdateMultiThreadedExample();
+  return 0;
 }
 
+void BasicExample() {
+  std::string buttonEntered;
 
-void BasicExample()
-{
-    std::string buttonEntered;
+  // You create the statemachine here, and pass on a starting state from an enum
+  // Note that only enum values are only valid here
+  StateMachine sm{State::stand};
 
-    // You create the statemachine here, and pass on a starting state from an enum
-    // Note that only enum values are only valid here
-    StateMachine sm{State::stand};
-    
-    // then you add your transitions
-    // you need to pass on a mathod that returns a bool
-    sm.AddTransition(State::stand, State::crouch, [&buttonEntered]{ return buttonEntered[0] == 'c'; })
-      .AddTransition(State::crouch, State::stand, [&buttonEntered]{ return buttonEntered[0] == 's'; });
+  // then you add your transitions
+  // you need to pass on a mathod that returns a bool
+  sm.AddTransition(State::stand, State::crouch, [&buttonEntered] {
+      return buttonEntered[0] == 'c';
+    }).AddTransition(State::crouch, State::stand, [&buttonEntered] {
+    return buttonEntered[0] == 's';
+  });
 
+  // loop for testing
+  for (;;) {
+    std::cin >> buttonEntered;
+    std::cout << "Typed: " << buttonEntered[0] << "\n";
 
-    // loop for testing
-    for (;;)
-    {
-        std::cin >> buttonEntered;
-        std::cout << "Typed: " << buttonEntered[0] << "\n";
-
-        sm.Update();
-        std::cout << "CurrentState: " << (int)sm.GetCurrentState() << "\n";
-    }
+    sm.Update();
+    std::cout << "CurrentState: " << (int)sm.GetCurrentState() << "\n";
+  }
 }
 
-void BasicWithUsingExample()
-{
-    std::string buttonEntered;
+void BasicWithUsingExample() {
+  std::string buttonEntered;
 
-    // you can also define your statemachines like this
-    // this can be put together next to your enum definitions 
-    using MyStateMachine = StateMachine<State>;
+  // you can also define your statemachines like this
+  // this can be put together next to your enum definitions
+  using MyStateMachine = StateMachine<State>;
 
-    // Here you create your defined statemachine
-    MyStateMachine sm{State::stand};
-    
-    // then you add your transitions
-    // you need to pass on a mathod that returns a bool
-    sm.AddTransition(State::stand, State::crouch, [&buttonEntered]{ return buttonEntered[0] == 'c'; })
-      .AddTransition(State::crouch, State::stand, [&buttonEntered]{ return buttonEntered[0] == 's'; });
+  // Here you create your defined statemachine
+  MyStateMachine sm{State::stand};
 
+  // then you add your transitions
+  // you need to pass on a mathod that returns a bool
+  sm.AddTransition(State::stand, State::crouch, [&buttonEntered] {
+      return buttonEntered[0] == 'c';
+    }).AddTransition(State::crouch, State::stand, [&buttonEntered] {
+    return buttonEntered[0] == 's';
+  });
 
-    // loop for testing
-    for (;;)
-    {
-        std::cin >> buttonEntered;
-        std::cout << "Typed: " << buttonEntered[0] << "\n";
+  // loop for testing
+  for (;;) {
+    std::cin >> buttonEntered;
+    std::cout << "Typed: " << buttonEntered[0] << "\n";
 
-        sm.Update();
-        std::cout << "CurrentState: " << (int)sm.GetCurrentState() << "\n";
-    }
+    sm.Update();
+    std::cout << "CurrentState: " << (int)sm.GetCurrentState() << "\n";
+  }
 }
 
-void StateCallBackExamples()
-{
-    std::string buttonEntered;
-    
-    // create statemachine
-    StateMachine sm{State::stand};
+void StateCallBackExamples() {
+  std::string buttonEntered;
 
-    // add transitions
-    sm.AddTransition(State::stand, State::crouch, [&buttonEntered]{ return buttonEntered[0] == 'c'; })
-      .AddTransition(State::crouch, State::stand, [&buttonEntered]{ return buttonEntered[0] == 's'; });
-    
-    // add state callbacks
-    // you also have the onUpdate and onStop callbacks
-    sm.AddOnStart(State::stand , []{ std::cout << "Stood up!\n"; })
-      .AddOnStart(State::crouch, []{ std::cout << "crouched!\n"; });
+  // create statemachine
+  StateMachine sm{State::stand};
 
+  // add transitions
+  sm.AddTransition(State::stand, State::crouch, [&buttonEntered] {
+      return buttonEntered[0] == 'c';
+    }).AddTransition(State::crouch, State::stand, [&buttonEntered] {
+    return buttonEntered[0] == 's';
+  });
 
-    // loop for testing
-    for (;;)
-    {
-        std::cin >> buttonEntered;
-        std::cout << "Typed: " << buttonEntered[0] << "\n";
+  // add state callbacks
+  // you also have the onUpdate and onStop callbacks
+  sm.AddOnStart(State::stand, [] {
+      std::cout << "Stood up!\n";
+    }).AddOnStart(State::crouch, [] { std::cout << "crouched!\n"; });
 
-        sm.Update();
-    }
+  // loop for testing
+  for (;;) {
+    std::cin >> buttonEntered;
+    std::cout << "Typed: " << buttonEntered[0] << "\n";
+
+    sm.Update();
+  }
 }
 
-void UpdateMultiThreadedExample()
-{
-    std::string buttonEntered;
+void UpdateMultiThreadedExample() {
+  std::string buttonEntered;
 
-    //look at previous examples
-    StateMachine sm{State::stand};
-    sm.AddStateContext(State::stand , []{ std::cout << "Stood up\n"; }, []{ std::cout << "Standing\n"; } , []{ std::cout << "Stopped standing\n"; })
-      .AddStateContext(State::crouch, []{ std::cout << "Crouched\n"; }, []{ std::cout << "crouching\n"; }, []{ std::cout << "Stopped crouching\n"; })
+  // look at previous examples
+  StateMachine sm{State::stand};
+  sm.AddStateContext(
+        State::stand, [] { std::cout << "Stood up\n"; },
+        [] { std::cout << "Standing\n"; },
+        [] { std::cout << "Stopped standing\n"; })
+      .AddStateContext(
+          State::crouch, [] { std::cout << "Crouched\n"; },
+          [] { std::cout << "crouching\n"; },
+          [] { std::cout << "Stopped crouching\n"; })
 
-      .AddTransition(State::stand , State::crouch, [&buttonEntered]{ return buttonEntered[0] == 'c'; })
-      .AddTransition(State::crouch, State::stand , [&buttonEntered]{ return buttonEntered[0] == 's'; });
+      .AddTransition(State::stand, State::crouch,
+                     [&buttonEntered] { return buttonEntered[0] == 'c'; })
+      .AddTransition(State::crouch, State::stand,
+                     [&buttonEntered] { return buttonEntered[0] == 's'; });
 
-
-    // you can also offload the updating to other threads
-    // this will keep updating that statemachine while your main thread can still do other stuff
-    std::thread thr{ [&sm] {
-      while(true)
-      {
-        sm.Update();
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-      }
-    }};
-    thr.detach();
-
-
-    // loop for testing
-    for (;;)
-    {
-        std::cin >> buttonEntered;
-        std::cout << "Typed: " << buttonEntered[0] << "\n";
+  // you can also offload the updating to other threads
+  // this will keep updating that statemachine while your main thread can still
+  // do other stuff
+  std::thread thr{[&sm] {
+    while (true) {
+      sm.Update();
+      std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
+  }};
+  thr.detach();
+
+  // loop for testing
+  for (;;) {
+    std::cin >> buttonEntered;
+    std::cout << "Typed: " << buttonEntered[0] << "\n";
+  }
 }
--- include/StateMachine.h
+++ include/StateMachine.h
@@ -1,143 +1,137 @@
 #include <functional>
 
-
 template <typename T>
-requires std::is_enum_v<T>
-class StateMachine
-{
-    struct TransitionContext
-    {
-        T to;
-        std::function<bool()> condition;
-    };
+  requires std::is_enum_v<T>
+class StateMachine {
 
-    struct StateContext
-    {
-        std::function<void()> onStart = []{};
-        std::function<void()> onUpdate = []{};
-        std::function<void()> onStop = []{};
+  struct TransitionContext {
+    T to;
+    std::function<bool()> condition;
+  };
 
-        std::vector<TransitionContext> transitions{};
-    };
-    
+  struct StateContext {
+    std::function<void()> onStart = [] {};
+    std::function<void()> onUpdate = [] {};
+    std::function<void()> onStop = [] {};
+
+    std::vector<TransitionContext> transitions{};
+  };
+
 public:
-    StateMachine(T startState);
-    ~StateMachine() = default;
-    StateMachine(const StateMachine&) = delete;
-    StateMachine(StateMachine&&) noexcept  = delete;
-    StateMachine& operator=(const StateMachine&) = delete;
-    StateMachine& operator=(StateMachine&&) noexcept  = delete;
+  StateMachine(T startState);
+  ~StateMachine() = default;
+  StateMachine(const StateMachine &) = delete;
+  StateMachine(StateMachine &&) noexcept = delete;
+  StateMachine &operator=(const StateMachine &) = delete;
+  StateMachine &operator=(StateMachine &&) noexcept = delete;
 
-    StateMachine& AddOnStart(T state, std::function<void()> onStart);
-    StateMachine& AddOnUpdate(T state, std::function<void()> onUpdate);
-    StateMachine& AddOnStop(T state, std::function<void()> onStop);
-    StateMachine& AddStateContext(T state, std::function<void()> onStart, std::function<void()> onUpdate, std::function<void()> onStop);
-    StateMachine& AddTransition(T from, T to, std::function<bool()> condition);
+  StateMachine &AddOnStart(T state, std::function<void()> onStart);
+  StateMachine &AddOnUpdate(T state, std::function<void()> onUpdate);
+  StateMachine &AddOnStop(T state, std::function<void()> onStop);
+  StateMachine &AddStateContext(T state, std::function<void()> onStart,
+                                std::function<void()> onUpdate,
+                                std::function<void()> onStop);
+  StateMachine &AddTransition(T from, T to, std::function<bool()> condition);
 
-    void Update();
-    T GetCurrentState() const { return mCurrentState; }
+  void Update();
+  T GetCurrentState() const { return mCurrentState; }
 
 private:
-    std::unordered_map<T, StateContext> mStateContexts;
-    T mCurrentState;
+  std::unordered_map<T, StateContext> mStateContexts;
+  T mCurrentState;
 
-    void SwitchToState(const TransitionContext& stateContext);
-    bool TransitionExists(const T& to, const std::vector<TransitionContext>& transitions);
+  void SwitchToState(const TransitionContext &stateContext);
+  bool TransitionExists(const T &to,
+                        const std::vector<TransitionContext> &transitions);
 };
 
-
-
+template <typename T>
+  requires std::is_enum_v<T>
+StateMachine<T>::StateMachine(T startState) : mCurrentState(startState) {}
 
 template <typename T>
-requires std::is_enum_v<T>
-StateMachine<T>::StateMachine(T startState)
-    : mCurrentState(startState)
-{
+  requires std::is_enum_v<T>
+StateMachine<T> &StateMachine<T>::AddOnStart(T state,
+                                             std::function<void()> onStart) {
+  mStateContexts[state].onStart = onStart;
+  return *this;
 }
 
 template <typename T>
-requires std::is_enum_v<T>
-StateMachine<T>& StateMachine<T>::AddOnStart(T state, std::function<void()> onStart)
-{
-    mStateContexts[state].onStart = onStart;
+  requires std::is_enum_v<T>
+StateMachine<T> &StateMachine<T>::AddOnUpdate(T state,
+                                              std::function<void()> onUpdate) {
+  mStateContexts[state].onUpdate = onUpdate;
+  return *this;
+}
+
+template <typename T>
+  requires std::is_enum_v<T>
+StateMachine<T> &StateMachine<T>::AddOnStop(T state,
+                                            std::function<void()> onStop) {
+  mStateContexts[state].onStop = onStop;
+  return *this;
+}
+
+template <typename T>
+  requires std::is_enum_v<T>
+StateMachine<T> &
+StateMachine<T>::AddStateContext(T state, std::function<void()> onStart,
+                                 std::function<void()> onUpdate,
+                                 std::function<void()> onStop) {
+  AddOnStart(state, onStart);
+  AddOnUpdate(state, onUpdate);
+  AddOnStop(state, onStop);
+  return *this;
+}
+
+template <typename T>
+  requires std::is_enum_v<T>
+StateMachine<T> &
+StateMachine<T>::AddTransition(T from, T to, std::function<bool()> condition) {
+  if (from == to)
     return *this;
-}
 
-template <typename T>
-requires std::is_enum_v<T>
-StateMachine<T>& StateMachine<T>::AddOnUpdate(T state, std::function<void()> onUpdate)
-{
-    mStateContexts[state].onUpdate = onUpdate;    
+  if (TransitionExists(to, mStateContexts[from].transitions))
     return *this;
+
+  mStateContexts[from].transitions.emplace_back(
+      TransitionContext{to, condition});
+  return *this;
 }
 
 template <typename T>
-requires std::is_enum_v<T>
-StateMachine<T>& StateMachine<T>::AddOnStop(T state, std::function<void()> onStop)
-{
-    mStateContexts[state].onStop = onStop;    
-    return *this;
+  requires std::is_enum_v<T>
+void StateMachine<T>::Update() {
+  for (const auto &stateContext : mStateContexts[mCurrentState].transitions) {
+    if (mCurrentState == stateContext.to)
+      continue;
+
+    if (!stateContext.condition())
+      continue;
+
+    SwitchToState(stateContext);
+    return;
+  }
+
+  mStateContexts[mCurrentState].onUpdate();
 }
 
 template <typename T>
-requires std::is_enum_v<T>
-StateMachine<T>& StateMachine<T>::AddStateContext(T state, std::function<void()> onStart, std::function<void()> onUpdate, std::function<void()> onStop)
-{
-    AddOnStart(state, onStart);
-    AddOnUpdate(state, onUpdate);
-    AddOnStop(state, onStop);
-    return *this;
+  requires std::is_enum_v<T>
+void StateMachine<T>::SwitchToState(const TransitionContext &stateContext) {
+  mStateContexts[mCurrentState].onStop();
+  mCurrentState = stateContext.to;
+  mStateContexts[mCurrentState].onStart();
 }
 
 template <typename T>
-requires std::is_enum_v<T>
-StateMachine<T>& StateMachine<T>::AddTransition(T from, T to, std::function<bool()> condition)
-{
-    if (from == to)
-        return *this;
-    
-    if (TransitionExists(to, mStateContexts[from].transitions))
-        return *this;
-    
-    mStateContexts[from].transitions.emplace_back(TransitionContext{to, condition});
-    return *this;
-}
+  requires std::is_enum_v<T>
+bool StateMachine<T>::TransitionExists(
+    const T &to, const std::vector<TransitionContext> &transitions) {
+  for (const auto &transition : transitions)
+    if (transition.to == to)
+      return true;
 
-template <typename T>
-requires std::is_enum_v<T>
-void StateMachine<T>::Update()
-{
-    for (const auto& stateContext : mStateContexts[mCurrentState].transitions)
-    {
-        if (mCurrentState == stateContext.to)
-            continue;
-
-        if (!stateContext.condition())
-            continue;
-
-        SwitchToState(stateContext);
-        return;
-    }   
-
-    mStateContexts[mCurrentState].onUpdate();
-}
-
-template <typename T>
-requires std::is_enum_v<T>
-void StateMachine<T>::SwitchToState(const TransitionContext& stateContext)
-{
-    mStateContexts[mCurrentState].onStop();
-    mCurrentState = stateContext.to;
-    mStateContexts[mCurrentState].onStart();
-}
-
-template <typename T>
-requires std::is_enum_v<T>
-bool StateMachine<T>::TransitionExists(const T& to, const std::vector<TransitionContext>& transitions)
-{
-    for (const auto& transition : transitions)
-        if (transition.to == to)
-            return true;
-
-    return false;
+  return false;
 }
